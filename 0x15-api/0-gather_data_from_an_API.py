268510a1266309ff7c565f761d0acab656c08853
OrangeB0lt <667@holbertonschool.com>
2019-09-16 17:17:07 -0400

    [System Engineering][API][init commit] initial commit for API project

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,29 +4,32 @@ Uses https://jsonplaceholder.typicode.com/ REST API, for a given
 employee ID, returns info on their TODO list prog
 '''
 import requests
-from sys import argv
+import sys
+
+
+
+def makeRequest(data, num):
+    ''' makes employee request and returns json dict response '''
+    rootURL = 'https://jsonplaceholder.typicode.com'
+    url = '{}{}{}'.format(rootURL, data, num)
+    return requests.get(url).json()
+
+
+def application(num):
+    ''' makes request for info about employee todo list, then prints '''
+    employee = makeRequest('/users/', num)
+    todos = makeRequest('/todos/?userId=', num)
+    completed = [i.get('title') for i in todos if i.get('completed')]
+    total = len(todos)
+    print('Employee {} is done with tasks({}/{}):'.format(
+        employee.get('name'), len(completed), total))
+    for i in completed:
+        print('\t {}'.format(i))
 
 
 if __name__ == '__main__':
-    employee_id = argv[1]
-    urlTodo = 'https://jsonplaceholder.typicode.com/todos/'
-    urlUser = 'https://jsonplaceholder.typicode.com/users/'
-    todo = requests.get(urlTodo, params={'userId': employee_id})
-    user = requests.get(urlUser, params={'id': employee_id})
-
-    todoDictList = todo.json()
-    userDictList = user.json()
-
-    doneTasks = []
-    totalTasks = len(todoDictList)
-    employee = userDictList[0].get('name')
-
-    for task in todoDictList:
-        if task.get('completed') is True:
-            doneTasks.append(task)
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee, len(doneTasks), totalTasks))
-
-    for task in doneTasks:
-        print("\t {}".format(task.get('title')))
+    """
+    MAIN App
+    """
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        application(sys.argv[1])
